a24d9f28fdbea239ed5758f2f7ec4db686a3988c
Russel_Recamunda <130855654+RusselJamesRecamunda@users.noreply.github.com>
2023-05-26 22:20:14 +0800

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,36 @@
+// Option 1: Link to a URL
+document.getElementById("button_elem").addEventListener("click", function() {
+    window.location.href = "https://www.schoolandcollegelistings.com/PH/Prieto-Diaz/1825525384148433/Talisayan-Elementary-School---114406";
+  });
+  
+  // Option 2: Call a JavaScript function
+  document.getElementById("button_elem").addEventListener("click", myFunction);
+  
+  function myFunction() {
+    // Perform some action here
+    console.log("Button clicked!");
+  }
+
+document.getElementById("button_hs").addEventListener("click", function() {
+    window.location.href = "https://snhs-sorsogoncity.jimdofree.com/";
+  });
+  document.getElementById("button_hs").addEventListener("click", myFunction);
+  function myFunction() {
+    console.log("Button clicked!");
+  }
+
+document.getElementById("button_Bumain").addEventListener("click", function() {
+    window.location.href = "https://bicol-u.edu.ph/";
+  });
+  document.getElementById("button_Bumain").addEventListener("click", myFunction);
+  function myFunction() {
+    console.log("Button clicked!");
+  }
+  
+  document.getElementById("button_bucs").addEventListener("click", function() {
+    window.location.href = "https://bicol-u.edu.ph/category/college-of-science-news-and-events/";
+  });
+  document.getElementById("button_bucs").addEventListener("click", myFunction);
+  function myFunction() {
+    console.log("Button clicked!");
+  }
